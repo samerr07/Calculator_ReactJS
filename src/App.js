cad7a80c1ed492effaa94ec7cb839e635e792f60
samerr07 <112320710+samerr07@users.noreply.github.com>
2022-11-02 18:37:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Calculator from "./components/Calculator";
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <Calculator />
+    </>
+  );
+}
+
+export default App;
